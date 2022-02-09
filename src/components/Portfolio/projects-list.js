797beb2c456d3410423b6767329a33d580c54856
Dave Quinn <dquinn8@cogeco.ca>
2022-02-09 16:22:56 -0500

    added description to all projects in project-list.js

--- src/components/Portfolio/projects-list.js
+++ src/components/Portfolio/projects-list.js
@@ -4,7 +4,8 @@ module.exports = [
         url: "https://savoury-subscriptions.herokuapp.com/",
         repo: "https://github.com/ScalexanderB/Savoury-Subscriptions",
         title: "Savoury Subscriptions",
-        tech: "MERN/GraphQl"
+        tech: "MERN/GraphQL/JavaScript",
+        description: "Demo meal delivery/subscription service single page web app."
 
     },
     {
@@ -12,7 +13,9 @@ module.exports = [
         url: "https://project-team-poseidon.herokuapp.com/",
         repo: "https://github.com/Qcent/project-poseidon",
         title: "Poseidon's Trunk",
-        tech: "Express/MVC/JavaScript"
+        tech: "Express/MVC/JavaScript",
+        description: "A buy & sell, classifieds, personal site for the people, by the people."
+
 
     },
     {
@@ -20,21 +23,27 @@ module.exports = [
         url: "https://qcent.github.io/pantry-remover/",
         repo: "https://github.com/Qcent/pantry-remover",
         title: "Pantry Remover",
-        tech: "ServerSide APIs/jQuery/CSS"
+        tech: "ServerSide APIs/jQuery/CSS",
+        description: "Find recipies based on the ingredients you have and build a list for the ones you don't."
+
     },
     {
         id: "project-blog-site",
         url: "https://qcent-blog-site.herokuapp.com/",
         repo: "https://github.com/Qcent/blog-site",
         title: "Blog Site",
-        tech: "Express/MVC/JavaScript"
+        tech: "Express/MVC/JavaScript",
+        description: "Post your thoughts then rate and comment on comunity posts."
+
     },
     {
         id: "project-robot-gladiators",
         url: "https://qcent.github.io/robot-gladiators/",
         repo: "https://github.com/Qcent/robot-gladiators",
         title: "Robot Gladiators",
-        tech: "jQuery/CSS/HTML"
+        tech: "jQuery/CSS/HTML",
+        description: "An exercise in providing a frontend experience to a simpe JavaScript demo."
+
     },
     {
         id: "project-poker",
@@ -42,41 +51,53 @@ module.exports = [
         norepo: true,
         repo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         title: "Texas Hold'Em",
-        tech: "JavaScript/Perl/CSS/HTML"
+        tech: "JavaScript/Perl/CSS/HTML",
+        description: "Play poker with friends in real time. Powered by a perl backend"
+
     },
     {
         id: "project-onepalm",
         url: "https://qcent.github.io/one-palm-solitaire/",
         repo: "https://github.com/Qcent/one-palm-solitaire",
         title: "One Palm Solitaire",
-        tech: "JavaScript/CSS/HTML"
+        tech: "JavaScript/CSS/HTML",
+        description: "A uniquie solitaire game written in JavaScript."
+
     },
     {
         id: "project-weather-forecast",
         url: "https://qcent.github.io/weather-forecast/",
         repo: "https://github.com/Qcent/weather-forecast",
         title: "Weather Forecaster",
-        tech: "APIs/JavaScript/CSS"
+        tech: "APIs/JavaScript/CSS",
+        description: "All weather, no ads! Why use any other weather app?"
+
     },
     {
         id: "project-jsquiz",
         url: "https://qcent.github.io/js-quiz-app/",
         repo: "https://github.com/Qcent/js-quiz-app",
         title: "JavaScript Quiz",
-        tech: "JavaScript/CSS/HTML"
+        tech: "JavaScript/CSS/HTML",
+        description: "A multiple choice quiz to test your JavaScript knowhow."
+
     },
     {
         id: "project-taskinator",
         url: "https://qcent.github.io/taskinator/",
         repo: "https://github.com/Qcent/taskinator",
         title: "Taskinator/Kanban",
-        tech: "JavaScript/Web APIs/HTML"
+        tech: "JavaScript/Web APIs/HTML",
+        description: "More then just a Todo list, It's a Kanban!"
+
     },
     {
         id: "project-jspassword",
         url: "https://qcent.github.io/js-password-generator/",
         repo: "https://github.com/Qcent/js-password-generator",
         title: "Password Generator",
-        tech: "JavaScript/CSS/HTML"
+        tech: "JavaScript/CSS/HTML",
+        description: "You set the requirements, I'll generate the password."
+
     },
 ]
